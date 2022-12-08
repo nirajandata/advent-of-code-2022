ed856210f2a990d4b9687dcfc8e27853782b2c55
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2022-12-08 04:19:23 -0500

    Create part2.cpp

---
+++ day 4/part2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int,int> calc(const string &c){
+	int x,y,pos;
+	pos=c.find('-');
+	x=stoi(c.substr(0,pos));
+	y=stoi(c.substr(pos+1,c.size()));
+	return make_pair(x,y);
+}
+int main(){
+	string s;
+	long long count=0;
+	while(getline(cin,s)){
+		string s1,s2;
+		s1=s.substr(0,s.find(','));
+		s2=s.substr(s.find(',')+1,s.size());
+		
+		int x1,y1,x2,y2;
+		tie(x1,y1)=calc(s1);
+		tie(x2,y2)=calc(s2);
+
+		if(y1>=x2 and y2>=x1){
+				count++;
+			}
+		
+	}
+	cout << count << endl;
+	return 0;
+}
