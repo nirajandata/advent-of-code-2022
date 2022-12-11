3052f9984ad839f65c624d4febd78f65ef7e9113
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2022-12-11 09:00:00 -0500

    edge case fixed

---
+++ day 6/part2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pos=0;
+bool present (vector<int> &v,char c){
+	int m=c-'a';
+	for(int i=0;i<v.size();i++){
+		if(m==v[i]){
+			pos=i+1;
+			return true;
+		}
+
+	}
+	return false;
+}
+int main(){
+	string s;
+	cin>>s;
+	vector<int> po;
+	for(int i=0;i<=(int) s.size();i++){
+		if(po.size()==14){
+			cout << i << endl;
+			break;
+		}
+		//setupto i=s.size for last checking 
+		if(s.size()==i) break;
+		if(po.size() && present(po,s[i])){
+			po.erase(po.begin(),po.begin()+pos);
+		}
+
+		po.push_back(s[i]-'a');
+	}
+	return 0;
+}
